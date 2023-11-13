d6133c1b03bd26f2ae35ac3fa15c154345872e0c
nijeraicevic <150294029+nijeraicevic@users.noreply.github.com>
2023-11-13 17:23:10 +0100

    Add files via upload

--- projekat/projekat/script.js
+++ projekat/projekat/script.js
@@ -1,13 +1,24 @@
 let x = 0;
 
-let br = document.getElementById('broj');
-br.textContent = x;
+
 
 function plus(){
     x++;
+    if(x>10){
+        alert("x ne sme da bude veci od 10");
+        x=0;
+    }
     br.textContent = x;
 }
 function minus(){
     x--;
+    if(x<0){
+        alert("x ne sme da bude manji od 0");
+        x=0;
+    }
     br.textContent = x;
 }
+
+
+let br = document.getElementById('broj');
+br.textContent = x;
